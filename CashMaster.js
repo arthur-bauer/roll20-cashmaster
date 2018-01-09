df01ae9b987d3b0e2f3ba0e973fa8e623fdb94d3
Arthur Bauer <arthurbauer@mac.com>
2018-01-09 23:39:23 +0100

    Implemented some of Aaron's code

--- CashMaster.js
+++ CashMaster.js
@@ -12,8 +12,10 @@ on('ready', function () {
           'use strict';
 
 		  var v="0.3a";
+		  var usd=0;
+		  var scname="CashMaster";
       
-		  log("Cashmaster v"+v+" online. Select one or more party members, then use `!cm -help` ");
+		  log(scname+" v"+v+" online. Select one or more party members, then use `!cm -help` ");
       
          on('chat:message', function(msg) {
           if (msg.type !== "api" && !playerIsGM(msg.playerid)) return;
@@ -29,27 +31,38 @@ on('ready', function () {
               }
               if (character) {
 	              partycounter++;
-                  var name = getAttrByName(character.id, "character_name");
-                  var pp = getAttrByName(character.id, "pp")*1;
-                  var gp = getAttrByName(character.id, "gp")*1;                  
-                  var ep = getAttrByName(character.id, "ep")*1;                  
-                  var sp = getAttrByName(character.id, "sp")*1;
-                  var cp = getAttrByName(character.id, "cp")*1;
+	              var name = getAttrByName(character.id, "character_name");
+                  var pp = getattr(character.id, "pp")*1;
+                  var gp = getattr(character.id, "gp")*1;                  
+                  var ep = getattr(character.id, "ep")*1;                  
+                  var sp = getattr(character.id, "sp")*1;
+                  var cp = getattr(character.id, "cp")*1;
                   var total = Math.round((pp*10+gp+ep*0.5+cp/100+sp/10)*10000)/10000;
                   partytotal = total+partytotal;
-                  output+= "<b>"+name+"</b><br>has "+pp+" platinum, "+gp+" gold, "+ep+" elektrum, "+sp+" silver, and "+cp+" copper.<br>Converted, this character has <span title='Equals roughly "+(total*25)+"USD'>"+total+" gp</span> in total.<hr>";
+                  output+= "<b>"+name+"</b><br>has ";
+                  if (pp>0) output+=pp+" platinum, ";
+                  if (gp>0) output+=gp+" gold, ";
+                  if (ep>0) output+=ep+" elektrum, ";
+                  if (sp>0) output+=sp+" silver,  ";
+                  if (cp>0) output+=cp+" copper.";
+                  
+                  output+="<br>Converted, this character has ";
+                  if (usd>0) output+="<span title='Equals roughly "+(total*usd)+"USD'>";
+                  output+=total+" gp";
+                  if (usd>0) output+="</span>";
+                  output+=" in total.<hr>";
               }
           });
           
           partytotal=Math.round(partytotal*100,0)/100;
           
           output+= "<b><u>Party total: "+partytotal+"</u></b>}}";
-          sendChat ("Cash master",output); 
+          sendChat (scname,output); 
 
           if (msg.content === "!cm -help")
 
 		  {
-			sendChat ("Cash master","/w gm <h2>Usage</h2><p>Select one or several party members.</p><p>Use</p><ul><li><code>!cm</code> to get an <strong>overview</strong> over the party’s cash,</li><li><code>!cmshare</code> to <strong>share</strong> the money equally between party members, converting the amount into the best combination of gold, silver and copper.</li><li><code>!cmconvert</code> to <strong>convert and share</strong> the money equally. between party members, converting the amount into the best combination of platinum, gold, elektrum, silver and copper.</li><li><code>!cmadd [amount][currency]</code> to add/substract money from the selected party members.</li></ul><h3>Examples</h3><ol><li><code>!cmadd -1gp 10sp</code> will substract 1gp and add 10 sp at the same time.</li><li><code>!cmshare</code> will collect all the money and share it evenly on the members, using gp, sp and cp only (pp and ep will be converted).</li></ol>");  
+			sendChat (scname,"/w gm <h2>Usage</h2><p>Select one or several party members.</p><p>Use</p><ul><li><code>!cm</code> to get an <strong>overview</strong> over the party’s cash,</li><li><code>!cmshare</code> to <strong>share</strong> the money equally between party members, converting the amount into the best combination of gold, silver and copper.</li><li><code>!cmconvert</code> to <strong>convert and share</strong> the money equally. between party members, converting the amount into the best combination of platinum, gold, elektrum, silver and copper.</li><li><code>!cmadd [amount][currency]</code> to add/substract money from the selected party members.</li></ul><h3>Examples</h3><ol><li><code>!cmadd -1gp 10sp</code> will substract 1gp and add 10 sp at the same time.</li><li><code>!cmshare</code> will collect all the money and share it evenly on the members, using gp, sp and cp only (pp and ep will be converted).</li></ol>");  
 			  
 		  }	
           
@@ -69,7 +82,7 @@ on('ready', function () {
               var cps=Math.round(rest);
               rest=(rest-cps)*partycounter;
               
-              sendChat ("Cash master","/w gm &{template:desc} {{desc=<b>Let's share this!</b><hr>Everyone receives the equivalent of <b>"+cashshare+" gp:</b> "+pps+" platinum, "+gps+" gold, "+eps+" elektrum, "+sps+" silver, and "+cps+" copper.}}");
+              sendChat (scname,"/w gm &{template:desc} {{desc=<b>Let's share this!</b><hr>Everyone receives the equivalent of <b>"+cashshare+" gp:</b> "+pps+" platinum, "+gps+" gold, "+eps+" elektrum, "+sps+" silver, and "+cps+" copper.}}");
 
               _.each(msg.selected, function(obj) {
               var token, character;
@@ -79,13 +92,13 @@ on('ready', function () {
                   character = getObj('character', token.get('represents'));
               }
               if (character) {
-                  setatt(character.id,"pp",pps);
-                  setatt(character.id,"gp",gps);
-                  setatt(character.id,"ep",eps);
-                  setatt(character.id,"sp",sps);
+                  setattr(character.id,"pp",pps);
+                  setattr(character.id,"gp",gps);
+                  setattr(character.id,"ep",eps);
+                  setattr(character.id,"sp",sps);
                   if (rest>0.999 && newcounter==partycounter) cps++;
                   if (rest<-0.999 && newcounter==partycounter) cps--;
-                  setatt(character.id,"cp",cps);
+                  setattr(character.id,"cp",cps);
               }
               
       });
@@ -107,7 +120,7 @@ on('ready', function () {
               var cps=Math.round(rest);
               rest=(rest-cps)*partycounter;
               
-              sendChat ("Cash master","/w gm &{template:desc} {{desc=<b>Let's share this!</b><hr>Everyone receives the equivalent of <b>"+cashshare+" gp:</b> "+pps+" platinum, "+gps+" gold, "+eps+" elektrum, "+sps+" silver, and "+cps+" copper.}}");
+              sendChat (scname,"/w gm &{template:desc} {{desc=<b>Let's share this!</b><hr>Everyone receives the equivalent of <b>"+cashshare+" gp:</b> "+pps+" platinum, "+gps+" gold, "+eps+" elektrum, "+sps+" silver, and "+cps+" copper.}}");
 
               _.each(msg.selected, function(obj) {
               var token, character;
@@ -117,13 +130,13 @@ on('ready', function () {
                   character = getObj('character', token.get('represents'));
               }
               if (character) {
-                  setatt(character.id,"pp",pps);
-                  setatt(character.id,"gp",gps);
-                  setatt(character.id,"ep",eps);
-                  setatt(character.id,"sp",sps);
+                  setattr(character.id,"pp",pps);
+                  setattr(character.id,"gp",gps);
+                  setattr(character.id,"ep",eps);
+                  setattr(character.id,"sp",sps);
                   if (rest>0.999 && newcounter==partycounter) cps++;
                   if (rest<-0.999 && newcounter==partycounter) cps--;
-                  setatt(character.id,"cp",cps);
+                  setattr(character.id,"cp",cps);
               }
               
       });
@@ -160,25 +173,25 @@ on('ready', function () {
               if (character) {
 				  partycounter++;
 	              var name = getAttrByName(character.id, "character_name");
-	              var pp = getAttrByName(character.id, "pp")*1;
-	              var gp = getAttrByName(character.id, "gp")*1;                  
-	              var ep = getAttrByName(character.id, "ep")*1;                  
-	              var sp = getAttrByName(character.id, "sp")*1;
-	              var cp = getAttrByName(character.id, "cp")*1;
+	              var pp = getattr(character.id, "pp")*1;
+	              var gp = getattr(character.id, "gp")*1;                  
+	              var ep = getattr(character.id, "ep")*1;                  
+	              var sp = getattr(character.id, "sp")*1;
+	              var cp = getattr(character.id, "cp")*1;
 	              var total = Math.round((pp*10+gp+ep*0.5+cp/100+sp/10)*10000)/10000;
 	              partytotal = total+partytotal;
 				  output+="<br><b>"+name+"</b>";
-                  if (ppa) {setatt(character.id,"pp",parseInt(pp)+parseInt(ppa[1])); output+="<br> "+ppa[0];}
-                  if (gpa) {setatt(character.id,"gp",parseInt(gp)+parseInt(gpa[1])); output+="<br> "+gpa[0];}
-                  if (epa) {setatt(character.id,"ep",parseInt(ep)+parseInt(epa[1])); output+="<br> "+epa[0];}
-                  if (spa) {setatt(character.id,"sp",parseInt(sp)+parseInt(spa[1])); output+="<br> "+spa[0];}
-                  if (cpa) {setatt(character.id,"cp",parseInt(cp)+parseInt(cpa[1])); output+="<br> "+cpa[0];}
+                  if (ppa) {setattr(character.id,"pp",parseInt(pp)+parseInt(ppa[1])); output+="<br> "+ppa[0];}
+                  if (gpa) {setattr(character.id,"gp",parseInt(gp)+parseInt(gpa[1])); output+="<br> "+gpa[0];}
+                  if (epa) {setattr(character.id,"ep",parseInt(ep)+parseInt(epa[1])); output+="<br> "+epa[0];}
+                  if (spa) {setattr(character.id,"sp",parseInt(sp)+parseInt(spa[1])); output+="<br> "+spa[0];}
+                  if (cpa) {setattr(character.id,"cp",parseInt(cp)+parseInt(cpa[1])); output+="<br> "+cpa[0];}
                   
                   
               }
               
 		      });
-              sendChat ("Cash master","/w gm &{template:desc} {{desc=<b>Cashing out - it's payday!</b><hr>Every selected character receives<br>"+output+"}}");
+              sendChat (scname,"/w gm &{template:desc} {{desc=<b>Cashing out - it's payday!</b><hr>"+output+"}}");
                       
       }
     
@@ -189,20 +202,25 @@ on('ready', function () {
 
 });
 
-function setatt(char_id, attr_name, newVal) {
-    var attribute = findObjs({
-		_type: "attribute",
-		_characterid: char_id,
-		_name: attr_name
-	})[0];
 
-	if (attribute == undefined) {
-		createObj("attribute", {
-		name: attr_name,
-		current: newVal,
-		characterid: char_id
-		});
-		} else {
-	attribute.set("current", newVal.toString());
-	}
+function getattr(cid,att)
+{
+let attr = findObjs({type:'attribute',characterid:cid,name:att})[0];
+if(attr){
+  let cur = attr.get('current'); // .get()
+//  log(`${att}: ${cur}`);
+  return cur;
+} 	
 }
+
+function setattr(cid,att,val)
+{
+let attr = findObjs({type:'attribute',characterid:cid,name:att})[0];
+if(attr){
+  let cur = attr.get('current'); // .get()
+  log(`${att}: ${cur}->${val}`);
+  attr.set({current: parseInt(val)}); // .set()
+} 	
+}
+
+
