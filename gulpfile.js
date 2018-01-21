815f262e88a1f433dbae8827252a26d1fbf7f1e1
Arthur Bauer <arthurbauer@mac.com>
2018-01-21 01:04:58 +0100

    Started with gulp routine

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var replace = require('gulp-string-replace');
+var gfi = require("gulp-file-insert");
+var markdown = require('gulp-markdown');
+var del = require('del');
+
+var p = require('./package.json')
+var version = p.version.replace('v','');
+console.log(version);
+
+var bump = require('gulp-bump');
+
+// Basic usage:
+// Will patch the version
+gulp.task('bump', function(){
+  gulp.src('./*.json')
+  .pipe(bump())
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-minor', function(){
+  gulp.src('./*.json')
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-major', function(){
+  gulp.src('./*.json')
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-pre', function(){
+  gulp.src('./*.json')
+  .pipe(bump({type:'prerelease'}))
+  .pipe(gulp.dest('./'));
+});
+
+
+
+gulp.task('readme1',function()
+	{
+    gulp.src('./README.md')
+        .pipe(markdown())
+	    .pipe(replace(new RegExp('\n', 'g'), ''))
+	    .pipe(replace(new RegExp('"', 'g'), '\''))
+        .pipe(gulp.dest('./tmp/script'))
+	
+	
+    gulp.src('./README.md')
+	    .pipe(replace(new RegExp('\n', 'g'), '\\r'))
+	    .pipe(replace(new RegExp('"', 'g'), '\''))
+        .pipe(gulp.dest('./tmp/package'))
+	
+	
+	});
+
+gulp.task('readme2',function()
+	{
+	gulp.src('./Cashmaster.js')
+    .pipe(replace(new RegExp('%%version%%', 'g'), version))
+	.pipe(gfi({
+	"%%README%%": "tmp/script/README.md",
+	}))  
+	.pipe(gulp.dest('./publish'))
+	.pipe(gulp.dest('./publish/'+version));
+
+	gulp.src('./prep/script.json')
+    .pipe(replace(new RegExp('%%version%%', 'g'), version))
+	.pipe(gfi({
+	"%%README%%": "tmp/package/README.md",
+	}))  
+	.pipe(gulp.dest('./publish'))
+	
+	});
+
+
+gulp.task('cleanup', function() {
+  return del.sync('./tmp');
+});
